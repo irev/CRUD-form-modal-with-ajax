1ba0d7d848e2827cad6cf12dfbd27d3eaf545f8b
Andra <AlexanderGFranglin@gmail.com>
2015-05-06 14:39:35 +0700

    UPDATE

--- process.php
+++ process.php
@@ -8,10 +8,11 @@
 // filename ; meedun.js
 /////////////////////////////////////
 include_once('db.php');
-if (!empty($_POST["namas"])&&!empty($_POST["umur"])) {
-  $namas=$_POST["namas"];
-  $umur=$_POST["umur"];
-  $query=mysql_query("INSERT INTO usr (nama,umur) values('$namas','$umur') ");
+//function funKirim(){
+if (!empty($_POST["namas"])&&!empty($_POST["umur"])&&isset($_POST['kirim'])) {
+    $namas=$_POST["namas"];
+    $umur=$_POST["umur"];
+    $query=mysql_query("INSERT INTO usr (nama,umur) values('$namas','$umur') ");
  
  // if($query){
     echo ' <div class="alert alert-success"> 
@@ -33,4 +34,33 @@ if (!empty($_POST["namas"])&&!empty($_POST["umur"])) {
                </ul>
                </div>';
   }
+//}
+
+function funEdit(){
+if (!empty($_POST["namas"])&&!empty($_POST["umur"])&&isset($_GET["edit"])) {
+      $namas=$_POST["namas"];
+      $umur=$_POST["umur"];
+      $query=mysql_query("INSERT INTO usr (nama,umur) values('$namas','$umur') ");
+ 
+ // if($query){
+    echo ' <div class="alert alert-success"> 
+       <ul class="fa-ul">
+           <li>
+           <i class="fa fa-exclamation-triangle fa-li fa-lg"></i>
+           <strong>Your data has been edit</strong> !  
+           </li>
+           </ul>
+           </div>';
+  }
+  else{
+        echo ' <div class="alert alert-danger"> 
+               <ul class="fa-ul">
+               <li>
+               <i class="fa fa-exclamation-triangle fa-li fa-lg"></i>
+               <strong>Error edit your data</strong> !  
+               </li>
+               </ul>
+               </div>';
+  }
+}
 ?>

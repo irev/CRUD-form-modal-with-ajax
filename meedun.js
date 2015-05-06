c9b2378595f59ca548694756ce7374be02d35699
Andra <refyandra@gmail.com>
2015-05-06 16:10:37 +0700

    Update meedun.js

--- meedun.js
+++ meedun.js
@@ -5,32 +5,57 @@
 // Date   : may 06 ,2015            //
 ////////////////////////////////////// 
 // filename ; meedun.js
-/////////////////////////////////////
-  $(document).ready(function(){
+/////////////Clear////////////////////////
+  $(document).ready(function cls(){
     $("#cls").click( function() {
        //$(this).val('');
+       $("#info").html('');
         $("#namas").val('');
         $("#umur").val('');
+
     });
   });
+  ////////////END CLEAR////////////
 /////////////INPUT ADD/////////////////
-               $(document).ready(function(){
-                    $("#kirim").click(function(){
-
+               $(document).ready(function funKirim(){
+                    $("#kirim").click(function()){
                         var namas=$("#namas").val();   
-                        var umur=$("#umur").val();  
-               
+                        var umur=$("#umur").val();
+                        //var kirim=$("#kirim").val('kirim');
                           $.ajax({
                               type:"post",
                               url:"process.php",
                               data:"namas="+namas+"&umur="+umur,
                               success:function(data){
                                  $("#info").html(data);
-                                 $("#namas").val('');
-                                 $("#umur").val('');
+                                 $("#namas").val('Tombol Save');
+                               //  $("#umur").val('');
                               }
                           });
  
                     });
                });
 ///////////////END ADD//////////////////////////
+
+////////////////EDIT///////////////////////////
+  $(document).ready(function funEdit(){
+    $("#edit").click( function() {
+       //$(this).val('');
+         var namas=$("#namas").val();
+         var namas=$("#umur").val();
+         var kirim=$("#kirim").val();
+                        $.ajax({
+                              type:"post",
+                              url:"process.php",
+                              data:"namas="+namas+"&umur="+umur,
+                              //data: "functionName=save",
+                              success:function(data){
+                                 $("#info").html(data);
+                                 $("#namas").val('Tombol Edit');
+                                 $("#umur").val('');
+                              }
+                          });
+ 
+
+    });
+  });
